83b1b814eb6d45564e16aeb58f38fcd7770e9c2b
Sumit98772 <112758077+Sumit98772@users.noreply.github.com>
2024-03-26 13:37:56 +0530

    Delete react directory

--- react/app.js
+++
@@ -1,7 +0,0 @@
-const parent =React.createElement("div",{id: "parent"},
-React.createElement("div",{id:"child"},
-React.createElement("h1",{},"I am H1 tag"),
-React.createElement("h1",{},"Here is 2nd one")));
-const root=ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
-
